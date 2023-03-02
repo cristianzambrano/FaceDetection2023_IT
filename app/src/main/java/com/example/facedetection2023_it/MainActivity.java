c399f994de94bc2cd20f0d0260915b092a392b13
Cristian <cristian_uteq@hotmail.com>
2023-03-01 22:12:31 -0500

    FOUR

--- app/src/main/java/com/example/facedetection2023_it/MainActivity.java
+++ app/src/main/java/com/example/facedetection2023_it/MainActivity.java
@@ -1,20 +1,29 @@
 package com.example.facedetection2023_it;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Size;
+import android.view.Surface;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.facedetection2023_it.ml.ModeloFlores;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.mlkit.vision.common.InputImage;
@@ -24,11 +33,20 @@ import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.TextRecognizerOptionsInterface;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.label.Category;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnSuccessListener<Text>, OnFailureListener {
+import CameraX.CameraConnectionFragment;
+import CameraX.ImageUtils;
+
+public class MainActivity extends AppCompatActivity implements OnSuccessListener<Text>, OnFailureListener,
+        ImageReader.OnImageAvailableListener {
     public static int REQUEST_CAMERA = 111;
     public static int REQUEST_GALLERY = 222;
 
@@ -44,7 +62,7 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageView = findViewById(R.id.image_view);
+        //mImageView = findViewById(R.id.image_view);
         txtResults = findViewById(R.id.txtresults);
         btCamera = findViewById(R.id.btCamera);
         btGaleria = findViewById(R.id.btGallery);
@@ -70,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
             } else if(permissions[i].equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE) ||
                     permissions[i].equals(Manifest.permission.READ_EXTERNAL_STORAGE)
             ) {
-                btGaleria.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);
+               // btGaleria.setEnabled(grantResults[i] == PackageManager.PERMISSION_GRANTED);//
             }
         }
     }
@@ -88,8 +106,8 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
                 if(permiso.equals(Manifest.permission.CAMERA))
                     btCamera.setEnabled(habilitado);
                 else if (permiso.equals(Manifest.permission.MANAGE_EXTERNAL_STORAGE)  ||
-                        permiso.equals(Manifest.permission.READ_EXTERNAL_STORAGE))
-                    btGaleria.setEnabled(habilitado);
+                        permiso.equals(Manifest.permission.READ_EXTERNAL_STORAGE)){}
+                    //btGaleria.setEnabled(habilitado);//
             }
         return list;
     }
@@ -100,8 +118,9 @@ public class MainActivity extends AppCompatActivity implements OnSuccessListener
     startActivityForResult(i, REQUEST_GALLERY);
 }
     public void abrirCamera (View view){
-    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    startActivityForResult(intent, REQUEST_CAMERA);
+        //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //startActivityForResult(intent, REQUEST_CAMERA);
+        this.setFragment();
 
 }
 
@@ -155,5 +174,158 @@ protected void onActivityResult(int requestCode, int resultCode, Intent data) {
             }
         }
         txtResults.setText(resultados);
+    }
+
+
+    public void PersonalizedModel(View v) {
+    try {
+        ModeloFlores model = ModeloFlores.newInstance(getApplicationContext());
+        TensorImage image = TensorImage.fromBitmap(mSelectedImage);
+
+        ModeloFlores.Outputs outputs = model.process(image);
+
+        List<Category> probability = outputs.getProbabilityAsCategoryList();
+        Collections.sort(probability, new CategoryComparator());
+
+        String res="";
+        for (int i = 0; i < probability.size(); i++) {
+            res = res + probability.get(i).getLabel() +  " " +  probability.get(i).getScore()*100 + " % \n";
+        }
+
+        txtResults.setText(res);
+        model.close();
+    } catch (IOException e) {
+        txtResults.setText("Error al procesar Modelo");
+    }
+}
+
+
+    int previewHeight = 0,previewWidth = 0;
+int sensorOrientation;
+@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+protected void setFragment() {
+    final CameraManager manager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+    String cameraId = null;
+    try {
+        cameraId = manager.getCameraIdList()[0];
+    } catch (CameraAccessException e) {
+        e.printStackTrace();
+    }
+    CameraConnectionFragment fragment;
+    CameraConnectionFragment camera2Fragment =
+            CameraConnectionFragment.newInstance(
+                    new CameraConnectionFragment.ConnectionCallback() {
+                        @Override
+                        public void onPreviewSizeChosen(final Size size, final int rotation) {
+                            previewHeight = size.getHeight();    previewWidth = size.getWidth();
+                            sensorOrientation = rotation - getScreenOrientation();
+                        }
+                    },
+                    this,   R.layout.camera_fragment,            new Size(640, 480));
+
+    camera2Fragment.setCamera(cameraId);
+    fragment = camera2Fragment;
+    getFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+}
+
+    protected int getScreenOrientation() {
+    switch (getWindowManager().getDefaultDisplay().getRotation()) {
+        case Surface.ROTATION_270:
+            return 270;
+        case Surface.ROTATION_180:
+            return 180;
+        case Surface.ROTATION_90:
+            return 90;
+        default:
+            return 0;
+    }
+}
+
+
+    @Override
+    public void onImageAvailable(ImageReader imageReader) {
+        if (previewWidth == 0 || previewHeight == 0)           return;
+    if (rgbBytes == null)    rgbBytes = new int[previewWidth * previewHeight];
+    try {
+        final Image image = imageReader.acquireLatestImage();
+        if (image == null)    return;
+        if (isProcessingFrame) {           image.close();            return;         }
+
+        isProcessingFrame = true;
+        final Image.Plane[] planes = image.getPlanes();
+        fillBytes(planes, yuvBytes);
+        yRowStride = planes[0].getRowStride();
+        final int uvRowStride = planes[1].getRowStride();
+        final int uvPixelStride = planes[1].getPixelStride();
+
+        imageConverter =  new Runnable() {
+                    @Override
+                    public void run() {
+                        ImageUtils.convertYUV420ToARGB8888( yuvBytes[0], yuvBytes[1], yuvBytes[2], previewWidth,  previewHeight,
+                yRowStride,uvRowStride, uvPixelStride,rgbBytes);
+                    }
+                };
+        postInferenceCallback =      new Runnable() {
+                    @Override
+                    public void run() {  image.close(); isProcessingFrame = false;  }
+                };
+
+        processImage();
+
+    } catch (final Exception e) {    }
+    
+    }
+
+    private void processImage() {
+    imageConverter.run();
+
+    rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Bitmap.Config.ARGB_8888);
+    rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
+
+    try {
+        ModeloFlores model = ModeloFlores.newInstance(getApplicationContext());
+        TensorImage image = TensorImage.fromBitmap(rgbFrameBitmap);
+
+        ModeloFlores.Outputs outputs = model.process(image);
+        List<Category> probability = outputs.getProbabilityAsCategoryList();
+        Collections.sort(probability, new CategoryComparator());
+        String res="";
+        for (int i = 0; i < probability.size(); i++) 
+            res = res + probability.get(i).getLabel() +  " " +  probability.get(i).getScore()*100 + " % \n";
+       
+        txtResults.setText(res);
+        model.close();
+    } catch (IOException e) {
+        txtResults.setText("Error al procesar Modelo");
+    }
+
+    postInferenceCallback.run();
+}
+    protected void fillBytes(final Image.Plane[] planes, final byte[][] yuvBytes) {
+	for (int i = 0; i < planes.length; ++i) {
+      		  final ByteBuffer buffer = planes[i].getBuffer();
+       	  if (yuvBytes[i] == null) {
+          		  yuvBytes[i] = new byte[buffer.capacity()];
+        	}
+        	buffer.get(yuvBytes[i]);
+    }
+}
+
+    private boolean isProcessingFrame = false;
+    private byte[][] yuvBytes = new byte[3][];
+    private int[] rgbBytes = null;
+    private int yRowStride;
+    private Runnable postInferenceCallback;
+    private Runnable imageConverter;
+    private Bitmap rgbFrameBitmap;
+
+    
+
+}
+
+class CategoryComparator implements java.util.Comparator<Category> {
+    @Override
+    public int compare(Category a, Category b) {
+        return (int)(b.getScore()*100) - (int)(a.getScore()*100);
     }
 }
